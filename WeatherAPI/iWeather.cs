083cef17c827d4d43bd811294c3e5abb4d5207fa
Ciorba Bogdan <bogdanciorba@ymail.com>
2021-03-22 21:43:25 +0200

    Main Code

--- WeatherAPI/iWeather.cs
+++ WeatherAPI/iWeather.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
-
-namespace WeatherAPI
+﻿namespace WeatherAPI
 {
     interface iWeather
     {
