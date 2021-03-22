07b9119dc859257a9a7f99d2329f17851215ee24
Ciorba Bogdan <bogdanciorba@ymail.com>
2021-03-22 22:29:53 +0200

    Main Code

--- WeatherAPI/WeatherGeneratorFromAPI.cs
+++ WeatherAPI/WeatherGeneratorFromAPI.cs
@@ -9,19 +9,19 @@ namespace WeatherAPI
     {
         static void Main()
         {
-            /** generating cityname and statecode as empty strings for later use **/
+            /* generating cityname and statecode as empty strings for later use */
             string CityName, StateCode;
             CityName = StateCode = string.Empty;
 
-            /** reads user keyboard input and formats it properly **/
+            /* reads user keyboard input and formats it properly */
             ReadInput(ref CityName, ref StateCode);
 
             WeatherForecast WeatherData = new WeatherForecast();
 
-            /** connects to API and generates WeatherData **/
+            /* connects to API and generates WeatherData */
             GetWeather(CityName, StateCode, ref WeatherData);
 
-            /** generates output messages **/
+            /* generates output messages */
             WorkWeatherData(CityName, WeatherData);
         }
 
@@ -48,16 +48,16 @@ namespace WeatherAPI
             {
                 StateName = Console.ReadLine();
             }
-            /** transforms state abbreviation (AR) to state code (US-AR) **/
+            /* transforms state abbreviation (AR) to state code (US-AR) */
             StateCode = GetStateCode(StateName);
         }
 
         public static string GetStateCode(string StateName)
         {
-            /** processing state abbreviation (AR) to state code (US-AR) **/
+            /* processing state abbreviation (AR) to state code (US-AR) */
             StateName = StateName.ToUpper();
 
-            /** openweather uses ISO 3166 codes, therefore we convert it to ISO 3166**/
+            /* openweather uses ISO 3166 codes, therefore we convert it to ISO 3166*/
             string StateCode = "US-" + StateName;
             return StateCode;
         }
@@ -67,16 +67,16 @@ namespace WeatherAPI
             string baseUrl = "";
             GenerateUrl(CityName, StateCode, ref baseUrl);
 
-            /** Downloading the JSON string to var json, then deserializing it as a dynamic object in var data; **/
+            /* Downloading the JSON string to var json, then deserializing it as a dynamic object in var data; */
             var json = Fetch(baseUrl).Result;
             var data = JsonConvert.DeserializeObject<dynamic>(json);
 
-            /** if there is data, generate it following the WeatherData object instructions **/
+            /* if there is data, generate it following the WeatherData object instructions */
             if (data != null)
             {
                 GenerateWeatherData(ref WeatherData, ref data);
             }
-            /** else, if there is no data, print "no data" **/
+            /* else, if there is no data, print "no data" **/
             else
             {
                 Console.WriteLine("No data");
@@ -85,31 +85,31 @@ namespace WeatherAPI
 
         public static void GenerateUrl(string CityName, string StateCode, ref string baseUrl)
         {   
-            /** fetching api key **/
+            /* fetching api key */
             string apiKey = "";
             apiKey = System.IO.File.ReadAllText(@"F:\Visual Studio Community Projects\WeatherAPI\api.txt");
             
-            /** generating baseUrl **/
+            /* generating baseUrl */
             string startUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
             
-            /** options must always have &appid= at the end**/
+            /* options must always have &appid= at the end */
             string options = "&units=imperial&appid=";
             baseUrl = startUrl + CityName + "," + StateCode + options + apiKey;
         }
 
         static async Task<string> Fetch(string url)
         {
-            /** fetching string from url**/
+            /* fetching string from url */
             using var client = new HttpClient();
             var content = await client.GetStringAsync(url);
 
-            /** returning value as string**/
+            /* returning value as string */
             return content;
         }
 
         public static void GenerateWeatherData(ref WeatherForecast WeatherData, ref dynamic data)
         {
-            /** transforming from data object to WeatherData object**/
+            /* transforming from data object to WeatherData object */
             WeatherData.temp = data.main.temp;
             WeatherData.winddegrees = data.wind.deg;
             WeatherData.weather = data.weather[0].main;
@@ -117,10 +117,10 @@ namespace WeatherAPI
 
         public static void WorkWeatherData(string CityName, WeatherForecast WeatherData)
         {
-            /** formatting helper, just shows City Name and weather **/
+            /* formatting helper, just shows City Name and weather */
             Console.WriteLine("\n"+CityName + " weather:");
 
-            /** prints temperature in fahrenheit and celsius **/
+            /* prints temperature in fahrenheit and celsius */
             DetermineTemperature(WeatherData.temp);
 
             /** input: wind degrees, wind direction
@@ -132,30 +132,30 @@ namespace WeatherAPI
 
             Console.WriteLine("The wind is currently blowing {0}.\n", winddirection);
 
-            /** based on the temperature and weather condition, sends a message that it is a nice day outside **/
+            /* based on the temperature and weather condition, sends a message that it is a nice day outside */
             DetermineNiceDay(WeatherData.temp, WeatherData.weather);
 
-            /** based on the temperature, sends a message that the user should use a coat **/
+            /* based on the temperature, sends a message that the user should use a coat */
             DetermineCoat(WeatherData.temp);
 
-            /** based on the value of the weather field, sends a message that the user should bring an umbrella **/
+            /* based on the value of the weather field, sends a message that the user should bring an umbrella */
             DetermineUmbrella(WeatherData.weather);
 
-            /** based on the value of the weather field, sends a message that it is cloudy **/
+            /* based on the value of the weather field, sends a message that it is cloudy */
             DetermineClouds(WeatherData.weather);
         }
 
         public static void DetermineTemperature(int temperature)
         {
-            /** conversion to celsius **/
+            /* conversion to celsius */
             float tempcelsius = (temperature - 32) * 5 / 9;
-            /** degrees fahrenheit + celsius print **/
+            /* degrees fahrenheit + celsius print */
             Console.WriteLine("It's currently " + temperature + " degrees Fahrenheit out there.\nThat's " + tempcelsius + " degrees Celsius.\n");
         }
 
         public static void DetermineNiceDay(int temperature, string weather)
         {
-            /** threshold for nice day message set as variable**/
+            /* threshold for nice day message set as variable */
             string nicedaythreshold = "Clear Sky";
 
             if (temperature > 60 && weather == nicedaythreshold)
@@ -166,7 +166,7 @@ namespace WeatherAPI
 
         public static void DetermineUmbrella (string weather)
         {
-            /** defining what bad weather is for umbrella message **/
+            /* defining what bad weather is for umbrella message */
             string[] badweather = { "snow", "rain", "extreme" };
             if (weather.ToLower() == badweather[0] || weather.ToLower() == badweather[1] || weather.ToLower() == badweather[2])
             {
@@ -176,7 +176,7 @@ namespace WeatherAPI
         
         public static void DetermineClouds (string weather)
         {
-            /** defining what clouds are for cloudy message **/
+            /* defining what clouds are for cloudy message */
             string cloudthreshold = "clouds";
             if (weather.ToLower() == cloudthreshold)
             {
@@ -186,7 +186,7 @@ namespace WeatherAPI
 
         public static void DetermineCoat (int temperature)
         {
-            /** defines what coat threshold is for coat message **/
+            /* defines what coat threshold is for coat message */
             int coatthreshold = 60;
             if (temperature < coatthreshold)
             {
@@ -196,52 +196,53 @@ namespace WeatherAPI
 
         public static void CalculateWindDirection(int winddegrees, ref string winddirection)
         {
-            /** checks which way the wind blows by comparing which part of the circle the orientation is**/
+            /* checks which way the wind blows by comparing which part of the circle the orientation is */
             if (winddegrees == 0)
             {
-                /** |^ **/
+                /* |^ */
                 winddirection = "North";
             }
             else if (winddegrees > 0 && winddegrees < 90)
             {
-                /** /^ **/
+                /* /^ */
                 winddirection = "North-East";
             }
             else if (winddegrees == 90)
             {
-                /** -> **/
+                /* -> */
                 winddirection = "East";
             }
             else if (winddegrees > 90 && winddegrees < 180)
             {
-                /** \v **/
+                /* \v */
                 winddirection = "South-East";
             }
             else if (winddegrees == 180)
             {
-                /** |v **/
+                /* |v */
                 winddirection = "South";
             }
             else if (winddegrees > 180 && winddegrees < 270)
             {
-                /** /v **/
+                /* /v */
                 winddirection = "South-West";
             }
             else if (winddegrees == 270)
             {
-                /** <- **/
+                /* <- */
                 winddirection = "West";
             }
             else if (winddegrees > 270 && winddegrees < 360)
             {
-                /** \^ **/
+                /* \^ */
                 winddirection = "North-West";
             }
             else if (winddegrees == 360)
             {
-                /** |^ **/
+                /* |^ */
                 winddirection = "North";
             }
+                /* in case of the wind value somehow being above 360 degrees */
             else winddirection = "Invalid wind direction data";
         }
     }
